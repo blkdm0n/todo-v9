50e6d286680a2cc2e681ec4fb5edd7cd21ae2763
Bernard Kitchens <bernard.kitchens@gmail.com>
2018-08-16 19:28:34 -0700

    all three requirementssatisfied each todo has an <li> each <li> has the todos text and completed status

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Version 9: Escape from the Console
 
 REQUIREMENTS
-[] There should be an <li> element for every todo
-[] Each <li> element should contain .todoText
-[] Each <li> element should contain .completed
+[X] There should be an <li> element for every todo
+[X] Each <li> element should contain .todoText
+[X] Each <li> element should contain .completed
 
--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 var todoList = {
-  todos: [],
-  displayTodos: function () {
-    console.log(`My Todos:`)
+  todos: [
+    {
+      todoText: `first`,
+      completed: false
+    },
+    {
+      todoText: `second`,
+      completed: true
+    }
+  ],
+  // displayTodos: function () {
+  //   console.log(`My Todos:`)
+  //   if (!this.todos.length) {
+  //     console.log(`Your todo list is empty!`);
+  //   } else {
+  //     for (let i = 0; i < this.todos.length; i++) {
+  //       if (this.todos[i].completed === true) {
+  //         console.log(`[X] ${this.todos[i].todoText}`);
+  //       } else {
+  //         console.log(`[ ] ${this.todos[i].todoText}`)
+  //       }
+  //     }
+  //   }
+  // },
+  displayTodos: function() {
+    //REQ #1 COMPLETE W/ PROBLEMS
+    //if list is empty how do i avoid repeat messages (same goes for the actual todos)
     if (!this.todos.length) {
-      console.log(`Your todo list is empty!`);
+      //create an h1
+      let node = document.createElement("h3");
+      //create a textNode to store our string
+      let textNode = document.createTextNode(`Your todo list is currently empty`);
+      //add the textNode to the node
+      node.appendChild(textNode);
+      //grab the todo-list id and append the node
+      document.getElementById(`empty-list`).appendChild(node);
     } else {
+      //loop through the array of todos
+      //create an <li> for each todo text
       for (let i = 0; i < this.todos.length; i++) {
+        let node = document.createElement("li");
         if (this.todos[i].completed === true) {
-          console.log(`[X] ${this.todos[i].todoText}`);
+          let textNode = document.createTextNode(`${this.todos[i].todoText}, completed: ${this.todos[i].completed}`);
+          node.appendChild(textNode);
+          document.getElementById(`todo-list`).appendChild(node);
         } else {
-          console.log(`[ ] ${this.todos[i].todoText}`)
+          let textNode = document.createTextNode(
+            `${this.todos[i].todoText.toUpperCase()} completed: ${this.todos[i].completed}`
+          );
+          node.appendChild(textNode);
+          document.getElementById(`todo-list`).appendChild(node);
         }
       }
     }
   },
-  addTodo: function (todoText) {
+  addTodo: function(todoText) {
     if (todoText) {
       this.todos.push({
         todoText: todoText,
-        completed: false,
+        completed: false
       });
     } else {
       console.log(`Please enter a valid todo`);
     }
     this.displayTodos();
   },
-  changeTodo: function (oldTodo, newTodo) {
+  changeTodo: function(oldTodo, newTodo) {
     for (let i = 0; i < this.todos.length; i++) {
       if (this.todos[i].todoText === oldTodo) {
         this.todos[i].todoText = newTodo;
@@ -33,7 +73,7 @@ var todoList = {
     }
     this.displayTodos();
   },
-  toggleCompleted: function (completedTodo) {
+  toggleCompleted: function(completedTodo) {
     for (let i = 0; i < this.todos.length; i++) {
       if (this.todos[i].todoText === completedTodo) {
         this.todos[i].completed = !this.todos[i].completed;
@@ -41,7 +81,7 @@ var todoList = {
     }
     this.displayTodos();
   },
-  toggleAll: function () {
+  toggleAll: function() {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
 
@@ -62,7 +102,7 @@ var todoList = {
     }
     this.displayTodos();
   },
-  deleteTodo: function (deletedTodo) {
+  deleteTodo: function(deletedTodo) {
     for (let i = 0; i < this.todos.length; i++) {
       if (this.todos[i].todoText === deletedTodo) {
         this.todos.splice(i, 1);
@@ -70,44 +110,38 @@ var todoList = {
     }
     this.displayTodos();
   }
-}
+};
 
 // HANDLERS OBJECT FOR SEPARATION OF CONCERNS
 
 const handlers = {
-  displayTodos: function () {
+  displayTodos: function() {
     todoList.displayTodos();
   },
-  toggleAll: function () {
+  toggleAll: function() {
     todoList.toggleAll();
   },
-  addTodo: function () {
-    let addTodoTextInput = document.getElementById("addTodoTextInput")
+  addTodo: function() {
+    let addTodoTextInput = document.getElementById("addTodoTextInput");
     todoList.addTodo(addTodoTextInput.value);
+    console.log(`ADD TODO TEXT  ${addTodoTextInput.value}`);
     addTodoTextInput.value = "";
   },
-  changeTodo: function () {
+  changeTodo: function() {
     let changeTodoCurrentText = document.getElementById("changeTodoCurrentText");
     let changeTodoNewText = document.getElementById("changeTodoNewText");
     todoList.changeTodo(changeTodoCurrentText.value, changeTodoNewText.value);
     changeTodoCurrentText.value = "";
     changeTodoNewText.value = "";
   },
-  deleteTodo: function () {
+  deleteTodo: function() {
     let deleteTodoText = document.getElementById("deleteTodoText");
     todoList.deleteTodo(deleteTodoText.value);
     deleteTodoText.value = "";
   },
-  toggleCompleted: function () {
+  toggleCompleted: function() {
     let completedTodoText = document.getElementById("completedTodoText");
     todoList.toggleCompleted(completedTodoText.value);
     completedTodoText.value = "";
-  },
-  // getKeyCode: function() {
-  //   //take the id
-  //   let child = event.target.id;
-  //   //find the parent
-  //   let parent = document.getElementById(child).parentElement;
-  //   console.log(parent);
-  // }
-}
+  }
+};
